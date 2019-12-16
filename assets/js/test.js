6b48d0985c55771eb8b964a5737883eb60bcb2b5
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2019-12-15 23:15:52 -0300

    test

--- assets/js/test.js
+++ assets/js/test.js
@@ -8,14 +8,21 @@ function loadPages() {
         if (this.readyState == 4 && this.status == 200) {
             var res = JSON.parse(this.responseText);
             const div = document.querySelector("#main-div");
+            const dropdownButton = document.querySelector("dropdown-button");
 
             // console.log(res);
 
             div.innerHTML = "<h2>Pages:</h2>";
 
             res.forEach(element => {
-                console.log(element.name);
+                // console.log(element.name);
                 div.innerHTML = div.innerHTML + '<p><a href=\"/dicionario-comandos-atalhos/pages/' + element.name.split(".")[0] + '.html\">' + element.name + '</a></p>';
+                
+                let a = document.createElement("a");
+                a.href = '/dicionario-comandos-atalhos/pages/' + element.name.split(".")[0] + '.html';
+                a.appendChild(document.createTextNode("Hello World"));
+                
+                dropdownButton.appendChild(a);
             });
         }
     };
